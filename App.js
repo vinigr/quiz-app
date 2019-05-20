82cba4a6cc44902bc7784a20f4f3caf5818aba66
Vinícios Grama <vinyirun1@gmail.com>
2019-05-19 23:54:27 -0300

    Adicionado Reactotron

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import NavigationRoot from './src/navigation/MainNavigator';
+import './ReactotronConfig';
 
 export default class App extends Component{
   render() {
