e1b0ae50550f23347e6546322604e2d1325da363
Vinícios Grama <vinyirun1@gmail.com>
2019-06-23 10:26:57 -0300

    Criação de arquivos de estilo com styled-components

---
+++ src/styles/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Button = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #059451;
+  margin-top: 15;
+  height: 50;
+  width: 99%;
+`;
+
+export const TitleAuth = styled.Text`
+  margin-top: 30;
+  margin-bottom: 20;
+  color: #000;
+  font-family: 'Rubik-Medium';
+  font-size: 35;
+`;
