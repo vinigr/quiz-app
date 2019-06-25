6414a99f8d26111026cb50e9f0f2296e28d45691
Vinícios Grama <vinyirun1@gmail.com>
2019-06-24 23:04:07 -0300

    Validação de token adicionada

--- src/screens/AuthLoading/index.js
+++ src/screens/AuthLoading/index.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import {
   ActivityIndicator,
-  AsyncStorage,
-  StatusBar,
   View,
 } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../../service/api';
 
 export default class AuthLoading extends Component {
-    componentDidMount = async () => {
-      const userToken = await AsyncStorage.getItem('userToken');
+  state={
+    loading: true,
+  }
 
-
-      this.props.navigation.navigate(userToken ? 'AppNavigator' : 'AuthNavigator');
-    };
-
-    render() {
-      return (
-        <View>
-          <ActivityIndicator />
-          <StatusBar barStyle="default" />
-        </View>
-      );
+  componentDidMount = async () => {
+    const userToken = await AsyncStorage.getItem('@QuizApp:token');
+    if (userToken) {
+      const user = await api.post('/verify', {
+        token: userToken,
+      });
+      if (user.data.auth === false) return this.props.navigation.navigate('authNavigator');
+      this.props.navigation.navigate('appNavigator');
+    } else {
+      this.props.navigation.navigate('authNavigator');
     }
+  };
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View>
+        {loading ? <ActivityIndicator /> : <View />}
+
+        {/* <StatusBar barStyle="default" /> */}
+      </View>
+    );
+  }
 }
