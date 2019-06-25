43d616afbd1d9ac33670829fa3ad4637683dc64d
Vinícios Grama <vinyirun1@gmail.com>
2019-06-25 16:17:47 -0300

    Design tela de perfil

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,53 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import {
+  View, StatusBar,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  Container, Header, Photo, Name, Menu, Option, OptionTextIcon, TextOption,
+} from './styles';
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Profile </Text>
-      </View>
-    );
-  }
+export default function Profile() {
+  return (
+    <View>
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" animated />
+      <Container>
+        <Header>
+          <Photo
+            source={{ uri: 'https://files.incrivel.club/files/news/part_71/715010/7826360-image-crop-1517x1585-1544022651-728-1680c03dbb-1545057856.jpg' }}
+          />
+          <Name>João Santana</Name>
+        </Header>
+        <Menu>
+          <Option>
+            <OptionTextIcon>
+              <Icon color="#4F4F4F" name="account-edit" size={30} />
+              <TextOption>Editar perfil</TextOption>
+            </OptionTextIcon>
+            <Icon name="chevron-right" size={30} />
+          </Option>
+          <Option>
+            <OptionTextIcon>
+              <Icon color="#4F4F4F" name="bell" size={30} />
+              <TextOption>Notificações</TextOption>
+            </OptionTextIcon>
+            <Icon name="chevron-right" size={30} />
+          </Option>
+          <Option>
+            <OptionTextIcon>
+              <Icon color="#4F4F4F" name="settings" size={30} />
+              <TextOption>Configurações</TextOption>
+            </OptionTextIcon>
+            <Icon name="chevron-right" size={30} />
+          </Option>
+          <Option>
+            <OptionTextIcon>
+              <Icon color="#4F4F4F" name="power-standby" size={30} />
+              <TextOption>Sair</TextOption>
+            </OptionTextIcon>
+          </Option>
+        </Menu>
+      </Container>
+    </View>
+  );
 }
---
+++ src/screens/Profile/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  background-color: #FAFAFA;
+  padding-top: 30px;
+  height: 100%;
+`;
+
+export const Header = styled.View`
+  
+`;
+
+export const Photo = styled.Image`
+  height: 150px;
+  width: 150px;
+  border-radius: 100px;
+  margin-bottom: 8px;
+`;
+
+export const Name = styled.Text`
+  font-family: 'Rubik-Medium';
+  font-size: 25px;
+  color: #272727;
+`;
+
+export const Menu = styled.View`
+  width: 95%;
+  margin-top: 35px;
+`;
+
+export const Option = styled.TouchableOpacity`
+  background-color: #F3F3F3;
+  width: 100%;
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 0 10px 0 10px;
+`;
+
+export const OptionTextIcon = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextOption = styled.Text`
+  font-family: 'Rubik-Regular';
+  margin-left: 10px;
+  font-size: 15px;
+  color: #4F4F4F;
+`;
