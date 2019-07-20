39a95d4b1ce153ac489e6b684808aea1a97634ce
Vinícios Grama <vinyirun1@gmail.com>
2019-07-19 21:11:06 -0300

    Correção arquivo que inicia as rotas

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import {
-  StatusBar,
-} from 'react-native';
 import NavigationRoot from './src/navigation/MainNavigator';
 import './ReactotronConfig';
 
 const App = () => (
   <>
-    <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
     <NavigationRoot />
   </>
 );
