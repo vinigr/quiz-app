09216e3c41da29fc71a704e7c934a87186053f7b
Vinícios Grama <vinyirun1@gmail.com>
2019-08-05 22:00:45 -0300

    Inicia página de conteúdo da disciplina

---
+++ src/screens/Disciplina/Conteudo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Conteudo(props) {
+  return (
+    <Container>
+      {console.tron.log(props)}
+    </Container>
+  );
+}
