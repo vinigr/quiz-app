11711102810836fceb93e56c7ff3d0ba965cc304
Vinícios Grama <vinyirun1@gmail.com>
2019-08-17 22:45:39 -0300

    Adiciona suporte a visualização de imagens no quiz

--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'react-native';
 
 import {
-  Container, StateQuestions, TextState, QuestionView, QuestionText,
+  Container, StateQuestions, TextState,
+  QuestionView, QuestionText, OptionsTF,
+  OptionTF, TextOption, OptionsME, OptionME, ImageQuestion,
 } from './styles';
 
 import api from '../../service/api';
@@ -12,32 +14,32 @@ import Loading from '../../components/Loading';
 
 export default function Quiz() {
   const [questions, setQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(2);
   const [loading, setLoading] = useState(true);
 
   const indexQuestion = currentQuestion - 1;
 
-  // function renderOptions(question) {
-  //   const arrayOptions = [];
-  //   arrayOptions.push(
-  //     question.option1,
-  //     question.option2,
-  //     question.option3,
-  //     question.option4,
-  //     question.option5,
-  //   );
-  //   Object.assign(question, { options: arrayOptions });
+  function renderOptions(question) {
+    const arrayOptions = [];
+    arrayOptions.push(
+      question.option1,
+      question.option2,
+      question.option3,
+      question.option4,
+      question.option5,
+    );
+    Object.assign(question, { options: arrayOptions });
 
-  //   const { options } = question;
+    const { options } = question;
 
-  //   return options.map(
-  //     (option, index) => option !== null && (
-  //         <DivOption key={index} position={index} answer={question.answer}>
-  //           <p>{option}</p>
-  //         </DivOption>
-  //       ),
-  //   );
-  // }
+    return options.map(
+      (option, index) => option !== null && (
+      <OptionME key={index}>
+        <TextOption>{option}</TextOption>
+      </OptionME>
+      ),
+    );
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -67,17 +69,37 @@ export default function Quiz() {
                   {console.tron.log(questions[indexQuestion])}
                   {questions[indexQuestion].tfQuestion.question}
                 </QuestionText>
+                {!questions[indexQuestion].tfQuestion.pathImage && (
+                  <ImageQuestion source={{ uri: 'https://s3.amazonaws.com/qcon-assets-production/images/provas/37927/imagem-026.jpg' }} />
+                )}
+                <OptionsTF>
+                  <OptionTF>
+                    <TextOption>Verdadeiro</TextOption>
+                  </OptionTF>
+                  <OptionTF>
+                    <TextOption>Falso</TextOption>
+                  </OptionTF>
+                </OptionsTF>
               </QuestionView>
             ) : (
               <QuestionView>
                 <QuestionText>
                   {questions[indexQuestion].meQuestion.question}
                 </QuestionText>
+                {!questions[indexQuestion].meQuestion.pathImage && (
+                  <ImageQuestion
+                    source={{ uri: 'https://www.infoescola.com/wp-content/uploads/2018/02/Clipboard01-763.jpg' }}
+                  />
+                )}
+                <OptionsME>
+                  {renderOptions(questions[indexQuestion].meQuestion)}
+                </OptionsME>
               </QuestionView>
             )}
           </>
         )
       }
+      <Button title="Anterior" onPress={() => setCurrentQuestion(currentQuestion - 1)} />
       <Button title="Próximo" onPress={() => setCurrentQuestion(currentQuestion + 1)} />
     </Container>
   );
--- src/screens/Quiz/styles.js
+++ src/screens/Quiz/styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import Image from 'react-native-scalable-image';
 
-export const Container = styled.View`
-  padding: 10px;
+export const Container = styled.ScrollView`
+  padding: 0 6px;
   width: 100%;
 `;
 
@@ -18,9 +20,44 @@ export const TextState = styled.Text`
 
 export const QuestionView = styled.View`
   width: 100%;
+  align-items: center;
 `;
 
 export const QuestionText = styled.Text`
   font-family: 'Rubik-Regular';
   font-size: 24px;
+  margin-bottom: 16px;
+`;
+
+export const ImageQuestion = styled(Image).attrs({
+  width: Dimensions.get('window').width,
+  height: 500,
+})`
+  margin-bottom: 20px;
+  border-radius: 4px;
+`;
+
+export const OptionsTF = styled.View`
+  width: 100%;
+  flex-direction: row;
+`;
+
+export const OptionTF = styled.TouchableHighlight`
+  width: 40%;
+`;
+
+export const TextOption = styled.Text`
+  font-size: 20px;
+`;
+
+export const OptionsME = styled.View`
+  width: 100%;
+`;
+
+export const OptionME = styled.TouchableHighlight`
+  width: 100%;
+  padding: 6px;
+  margin-bottom: 10px;
+  background-color: aliceblue;
+  border-radius: 2px;
 `;
