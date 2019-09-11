0c675f3f4f41aba5967f49e4d0e928c04abe76f3
Vinícios Grama <vinyirun1@gmail.com>
2019-09-11 16:51:54 -0300

    Adiciona funcionalidade para cancelar inscrição em disciplina

--- src/screens/Disciplinas/index.js
+++ src/screens/Disciplinas/index.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import {
-  FlatList, TouchableOpacity, ToastAndroid,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Modal from 'react-native-modal';
+import React, { useState, useEffect } from "react";
+import { FlatList, TouchableOpacity, ToastAndroid } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Modal from "react-native-modal";
 import {
   Container,
   IconsHeader,
@@ -24,24 +22,25 @@ import {
   TextModalAdd,
   InputCode,
   ButtonAdd,
-  TextAdd,
-} from './styles';
+  TextAdd
+} from "./styles";
 
-import Loading from '../../components/Loading';
+import Loading from "../../components/Loading";
 
-import api from '../../service/api';
+import api from "../../service/api";
 
 export default function Disciplinas(props) {
   const [subject, setSubject] = useState([]);
   const [visible, setVisible] = useState(false);
   const [modalAdd, setModalAdd] = useState(false);
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
+  const [subjectSelected, setSubjectSelected] = useState();
   const [loading, setLoading] = useState(true);
 
   async function fetchData() {
     try {
       setLoading(true);
-      const subjects = await api.get('/user/subjects');
+      const subjects = await api.get("/user/subjects");
       await setSubject(subjects.data.subjects);
       return setLoading(false);
     } catch (error) {
@@ -50,28 +49,32 @@ export default function Disciplinas(props) {
   }
 
   useEffect(() => {
-    props.navigation.setParams({ openModalSubject: () => setModalAdd(true), update: fetchData });
+    props.navigation.setParams({
+      openModalSubject: () => setModalAdd(true),
+      update: fetchData
+    });
     fetchData();
   }, []);
 
   async function addSubject() {
-    if (!code || code === '') return setModalAdd(false);
+    if (!code || code === "") return setModalAdd(false);
 
     await setModalAdd(false);
     try {
-      const subjectRegister = await api.post('/subject/registrationUser', {
-        accessCode: code,
+      const subjectRegister = await api.post("/subject/registrationUser", {
+        accessCode: code
       });
       await setModalAdd(false);
+      await setCode(null);
 
       const tempSubjects = [...subject];
       tempSubjects.push(subjectRegister.data);
       setSubject(tempSubjects);
 
       return ToastAndroid.showWithGravity(
-        'Cadastro feito com sucesso!',
+        "Cadastro feito com sucesso!",
         ToastAndroid.SHORT,
-        ToastAndroid.CENTER,
+        ToastAndroid.CENTER
       );
     } catch (error) {
       await setModalAdd(false);
@@ -79,99 +82,132 @@ export default function Disciplinas(props) {
         return ToastAndroid.showWithGravity(
           `${error.response.data.message}`,
           ToastAndroid.LONG,
-          ToastAndroid.CENTER,
+          ToastAndroid.CENTER
         );
       }
       return ToastAndroid.showWithGravity(
-        'Erro ao cadastrar!',
+        "Erro ao cadastrar!",
         ToastAndroid.LONG,
-        ToastAndroid.CENTER,
+        ToastAndroid.CENTER
       );
     }
   }
 
+  function handleVisibleSubscribe(id) {
+    setVisible(true);
+    setSubjectSelected(id);
+  }
+
+  async function unsubscribe() {
+    if (!setSubjectSelected) return console.tron.log("erro");
+
+    try {
+      const { data } = await api.delete(`/user/unsubscribe/${subjectSelected}`);
+      const tempSubjects = [...subject];
+      const newSubjects = tempSubjects.filter(
+        subject => subject.subject.id !== subjectSelected
+      );
+      setSubject(newSubjects);
+
+      ToastAndroid.showWithGravity(
+        data.message,
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER
+      );
+    } catch (error) {
+      ToastAndroid.showWithGravity(
+        "Erro ao cancelar inscrição!",
+        ToastAndroid.LONG,
+        ToastAndroid.CENTER
+      );
+    }
+    setVisible(false);
+    setSubjectSelected(null);
+  }
+
   return (
     <Container>
-      {loading ? <Loading />
-        : (
-          <ViewList>
-            <FlatList
-              data={subject}
-              keyExtractor={item => `${item.subject_id}`}
-              onRefresh={fetchData}
-              refreshing={loading}
-              renderItem={({ item }) => (
-                <ItemList
-                  onPress={() => props.navigation.navigate('Disciplina', {
-                    id: item.subject_id,
+      {loading ? (
+        <Loading />
+      ) : (
+        <ViewList>
+          <FlatList
+            data={subject}
+            keyExtractor={item => `${item.subject_id}`}
+            onRefresh={fetchData}
+            refreshing={loading}
+            renderItem={({ item }) => (
+              <ItemList
+                onPress={() =>
+                  props.navigation.navigate("Disciplina", {
+                    id: item.subject.id
                   })
-                  }
-                  style={{
-                    shadowColor: '#000',
-                    shadowOffset: {
-                      width: 0,
-                      height: 3,
-                    },
-                    shadowOpacity: 0.27,
-                    shadowRadius: 4.65,
+                }
+                style={{
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 3
+                  },
+                  shadowOpacity: 0.27,
+                  shadowRadius: 4.65,
 
-                    elevation: 6,
-                  }}
-                >
-                  <HeaderSubject>
-                    <ViewSubject>
-                      <TitleItem>{item.subject.name}</TitleItem>
-                      <TopicItem>{item.subject.topic}</TopicItem>
-                    </ViewSubject>
-                    <TouchableIcon
-                      onPress={() => setVisible(true)}
-                    >
-                      <Icon name="dots-vertical" size={24} />
-                    </TouchableIcon>
-                  </HeaderSubject>
-                  <ViewTeacher>
-                    <NameTeacher>{item.subject.user.name}</NameTeacher>
-                  </ViewTeacher>
-                </ItemList>
-              )}
-            />
-            <Modal
-              isVisible={visible}
-              onBackButtonPress={() => setVisible(false)}
-              onBackdropPress={() => setVisible(false)}
-              animationIn="fadeIn"
-              animationOut="fadeOut"
-              animationInTiming={200}
-              backdropTransitionOutTiming={0}
-            >
-              <ViewModal>
-                <OptionsModal onPress={() => {}}>
-                  <OptionsText>Cancelar inscrição</OptionsText>
-                </OptionsModal>
-              </ViewModal>
-            </Modal>
-            <Modal
-              isVisible={modalAdd}
-              onBackButtonPress={() => setModalAdd(false)}
-              onBackdropPress={() => setModalAdd(false)}
-              animationIn="fadeIn"
-              animationOut="fadeOut"
-              animationInTiming={200}
-              backdropTransitionOutTiming={0}
-            >
-              <ViewModalAdd>
-                <TextModalAdd>
+                  elevation: 6
+                }}
+              >
+                <HeaderSubject>
+                  <ViewSubject>
+                    <TitleItem>{item.subject.name}</TitleItem>
+                    <TopicItem>{item.subject.topic}</TopicItem>
+                  </ViewSubject>
+                  <TouchableIcon
+                    onPress={() => handleVisibleSubscribe(item.subject.id)}
+                  >
+                    <Icon name="dots-vertical" size={24} />
+                  </TouchableIcon>
+                </HeaderSubject>
+                <ViewTeacher>
+                  <NameTeacher>{item.subject.user.name}</NameTeacher>
+                </ViewTeacher>
+              </ItemList>
+            )}
+          />
+          <Modal
+            isVisible={visible}
+            onBackButtonPress={() => setVisible(false)}
+            onBackdropPress={() => setVisible(false)}
+            animationIn="fadeIn"
+            animationOut="fadeOut"
+            animationInTiming={200}
+            backdropTransitionOutTiming={0}
+          >
+            <ViewModal>
+              <OptionsModal onPress={() => unsubscribe()}>
+                <OptionsText>Cancelar inscrição</OptionsText>
+              </OptionsModal>
+            </ViewModal>
+          </Modal>
+          <Modal
+            isVisible={modalAdd}
+            onBackButtonPress={() => setModalAdd(false)}
+            onBackdropPress={() => setModalAdd(false)}
+            animationIn="fadeIn"
+            animationOut="fadeOut"
+            animationInTiming={200}
+            backdropTransitionOutTiming={0}
+          >
+            <ViewModalAdd>
+              <TextModalAdd>
                 Peça o código para o criador da disciplina e digite-o abaixo:
-                </TextModalAdd>
-                <InputCode value={code} onChangeText={text => setCode(text)} />
-                <ButtonAdd onPress={() => addSubject()}>
-                  <TextAdd>Adicionar</TextAdd>
-                </ButtonAdd>
-              </ViewModalAdd>
-            </Modal>
-          </ViewList>
-        )
-      }
+              </TextModalAdd>
+              <InputCode value={code} onChangeText={text => setCode(text)} />
+              <ButtonAdd onPress={() => addSubject()}>
+                <TextAdd>Adicionar</TextAdd>
+              </ButtonAdd>
+            </ViewModalAdd>
+          </Modal>
+        </ViewList>
+      )}
     </Container>
   );
 }
@@ -179,25 +215,12 @@ export default function Disciplinas(props) {
 Disciplinas.navigationOptions = ({ navigation }) => ({
   headerRight: (
     <IconsHeader>
-      <IconHeader
-        onPress={() => navigation.state.params.update()}
-      >
-        <Icon
-          name="refresh"
-          size={30}
-          color="#000"
-        />
+      <IconHeader onPress={() => navigation.state.params.update()}>
+        <Icon name="refresh" size={30} color="#000" />
       </IconHeader>
-      <IconHeader
-        onPress={() => navigation.state.params.openModalSubject()}
-      >
-        <Icon
-          name="plus"
-          size={30}
-          color="#000"
-        />
+      <IconHeader onPress={() => navigation.state.params.openModalSubject()}>
+        <Icon name="plus" size={30} color="#000" />
       </IconHeader>
     </IconsHeader>
-
-  ),
+  )
 });
