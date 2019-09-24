c0efce32d183057a9a6b10080b18a023eeb60d35
Vinícios Grama <vinyirun1@gmail.com>
2019-09-24 14:21:04 -0300

    :construction: Inicia opção de disputar quiz sem login

--- src/screens/Abertura/index.js
+++ src/screens/Abertura/index.js
@@ -1,27 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Modal from 'react-native-modal';
 import {
-  Container, Header, ImageLogo, Title, InputView, Button, TextButton,
+  Container,
+  Header,
+  ImageLogo,
+  Title,
+  InputView,
+  Button,
+  TextButton,
+  TextButtonQuiz,
+  ViewModal,
+  Input,
+  TextModal,
+  ButtonModal,
+  TextButtonAdd,
 } from './styles';
 
+import { TextError } from '../../styles';
 
-const Abertura = props => (
-  <Container>
-    <Header>
-      <ImageLogo
-        source={require('../../assets/images/logo.png')}
-      />
-      <Title>Bem-vindo</Title>
-    </Header>
-    <InputView>
-      <Button onPress={() => props.navigation.navigate('Cadastro')}>
-        <TextButton>Cadastrar</TextButton>
-      </Button>
-      <Button onPress={() => props.navigation.navigate('Login')}>
-        <TextButton>Entrar</TextButton>
-      </Button>
-    </InputView>
-  </Container>
-);
+import api from '../../service/api';
 
-export default Abertura;
+export default function Abertura(props) {
+  const [modalAdd, setModalAdd] = useState(false);
+  const [code, setCode] = useState('');
+  const [name, setName] = useState();
+  const [quiz, setQuiz] = useState();
+  const [error, setError] = useState();
+
+  async function searchQuiz() {
+    setError(null);
+    try {
+      const { data } = await api.get(`/unloggedUser/${code}`);
+      console.tron.log(data);
+      setQuiz(data.quiz);
+    } catch ({ response }) {
+      setError(response.data.message);
+      console.tron.log(response);
+    }
+  }
+
+  async function startQuiz() {
+    setError(null);
+    if (!name) return setError('Nome não informado!');
+    if (!quiz) return setError('Quiz não informado!');
+    try {
+      const { data } = await api.post('/unloggedUser/startQuiz', {
+        name,
+        quiz,
+      });
+      setCode(null);
+      setQuiz(null);
+      console.tron.log(data);
+    } catch ({ response }) {
+      setError(response.data.message);
+      console.tron.log(response);
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <ImageLogo source={require('../../assets/images/logo.png')} />
+        <Title>Bem-vindo</Title>
+      </Header>
+      <InputView>
+        <Button onPress={() => props.navigation.navigate('Cadastro')}>
+          <TextButton>Cadastrar</TextButton>
+        </Button>
+        <Button onPress={() => props.navigation.navigate('Login')}>
+          <TextButton>Entrar</TextButton>
+        </Button>
+        <Button onPress={() => setModalAdd(true)}>
+          <TextButtonQuiz>Disputar sem logar</TextButtonQuiz>
+        </Button>
+      </InputView>
+      <Modal
+        isVisible={modalAdd}
+        onBackButtonPress={() => setModalAdd(false)}
+        onBackdropPress={() => setModalAdd(false)}
+        animationIn="fadeIn"
+        animationOut="fadeOut"
+        animationInTiming={200}
+        backdropTransitionOutTiming={0}
+      >
+        <ViewModal>
+          {!quiz ? (
+            <>
+              <TextModal>Código do quiz</TextModal>
+              <Input
+                placeholder="Código"
+                maxLength={8}
+                autoCapitalize="characters"
+                value={code}
+                onChangeText={text => setCode(text)}
+              />
+              {error && <TextError>{error}</TextError>}
+              <ButtonModal onPress={() => searchQuiz()}>
+                <TextButtonAdd>Buscar</TextButtonAdd>
+              </ButtonModal>
+            </>
+          ) : (
+            <>
+              <TextModal>Digite seu nome</TextModal>
+              <Input placeholder="Nome" value={name} onChangeText={text => setName(text)} />
+              {error && <TextError>{error}</TextError>}
+              <ButtonModal onPress={() => startQuiz()}>
+                <TextButtonAdd>Entrar</TextButtonAdd>
+              </ButtonModal>
+            </>
+          )}
+        </ViewModal>
+      </Modal>
+    </Container>
+  );
+}
--- src/screens/Abertura/styles.js
+++ src/screens/Abertura/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 import Fonts from '../../utils/fonts';
 
 export const Container = styled.View`
@@ -42,4 +43,50 @@ export const TextButton = styled.Text`
   color: #fff;
   font-family: ${Fonts.RubikRegular};
   font-size: 25;
+`;
+
+export const TextButtonQuiz = styled.Text`
+  color: #fff;
+  font-family: ${Fonts.RubikRegular};
+  font-size: 16;
+`;
+
+export const ViewModal = styled.View`
+  background-color: #fff;
+  width: ${Dimensions.get('window').width * 0.8};
+  height: ${Dimensions.get('window').height * 0.3};
+  margin: 0 auto;
+  padding: 0 6%;
+  align-items: center;
+  border-radius: 5px;
+  justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 16px;
+  color: #000;
+  height: 50px;
+  width: 100%;
+  border: solid #9c9c9c 1px;
+  border-radius: 4px;
+  margin-bottom: 16px;
+  background-color: #e3e3e3;
+`;
+
+export const TextModal = styled.Text`
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
+export const ButtonModal = styled.TouchableOpacity`
+  padding: 10px;
+  background-color: #e7e7e7;
+  border-radius: 4px;
+  align-self: flex-end;
+  border: solid #9c9c9c 0.5px;
+`;
+
+export const TextButtonAdd = styled.Text`
+  font-size: 16px;
+  color: #000;
 `;
