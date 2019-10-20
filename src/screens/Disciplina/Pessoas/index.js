13ff43f78278ab867ec569113206667c38035bf5
Vinícios Grama <vinyirun1@gmail.com>
2019-10-20 00:10:41 -0300

    :children_crossing: Remove jogador da lista de outros usuários

--- src/screens/Disciplina/Pessoas/index.js
+++ src/screens/Disciplina/Pessoas/index.js
@@ -10,21 +10,25 @@ import Loading from '../../../components/Loading';
 import imagePerson from '../../../assets/images/default-person.png';
 
 import api from '../../../service/api';
+import AuthService from '../../../service/auth';
 
 export default function Disciplina(props) {
   const [loading, setLoading] = useState(true);
+  const [teacher, setTeacher] = useState();
   const [peoples, setPeoples] = useState([]);
 
   useEffect(() => {
     const id = props.navigation.getParam('id');
-
     const fetchData = async () => {
       try {
-        const result = await api.get(`/subject/users/${id}`);
-        await setPeoples(result.data);
+        const { data } = await api.get(`/subject/users/${id}`);
+        const { userId } = await AuthService.getProfile();
+        const peoplesData = data.usersSubject.filter(people => people.user_id !== userId);
+        await setTeacher(data.subject.user.name);
+        await setPeoples(peoplesData);
         return setLoading(false);
-      } catch (error) {
-        return console.tron.log(error.response);
+      } catch ({ response }) {
+        return console.tron.log(response.data);
       }
     };
 
@@ -40,7 +44,7 @@ export default function Disciplina(props) {
           <Title>Professor</Title>
           <User>
             <ImageUser source={imagePerson} />
-            <TextUser>{peoples.subject.user.name}</TextUser>
+            <TextUser>{teacher}</TextUser>
           </User>
           <Title>Colegas</Title>
           <FlatList
@@ -55,7 +59,6 @@ export default function Disciplina(props) {
           />
         </>
       )}
-
     </Container>
   );
 }
