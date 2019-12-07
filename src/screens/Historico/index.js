5ff0fe31a5a528c4d786babe76c9878758efc7f0
Vinícios Grama <vinyirun1@gmail.com>
2019-12-07 10:19:17 -0300

    :children_crossing: Melhora tela de histórico

--- src/screens/Historico/index.js
+++ src/screens/Historico/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList} from 'react-native';
 
-import { formatToTimeZone } from 'date-fns-timezone';
+import {formatToTimeZone} from 'date-fns-timezone';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   Container,
@@ -17,6 +17,12 @@ import {
 } from './styles';
 
 import api from '../../service/api';
+import {
+  ViewNotQuiz,
+  ButtonUpdate,
+  TextUpdate,
+  TextNotQuiz,
+} from '../Questionarios/styles';
 
 export default function Histórico(props) {
   const [disputes, setDisputes] = useState([]);
@@ -25,8 +31,10 @@ export default function Histórico(props) {
   async function fetchData() {
     try {
       setLoading(true);
-      const { data } = await api.post('/disputesList');
-      setDisputes(data.disputes);
+      const {data} = await api.post('/disputesList');
+      if (data.disputes) {
+        setDisputes(data.disputes);
+      }
     } catch (error) {
       console.tron.log(error);
     }
@@ -40,36 +48,48 @@ export default function Histórico(props) {
   return (
     <Container>
       <ViewList>
-        <FlatList
-          data={disputes}
-          keyExtractor={item => `${item.id}`}
-          onRefresh={fetchData}
-          refreshing={loading}
-          renderItem={({ item }) => (
-            <ItemList>
-              <ViewLeft>
-                <TitleItem>{item.Quiz.name}</TitleItem>
-                <DateDispute>
-                  {formatToTimeZone(item.createdAt, 'DD/MM/YY | HH:mm', {
-                    timeZone: 'America/Sao_Paulo',
-                  })}
-                </DateDispute>
-              </ViewLeft>
-              <ViewRight>
-                <ViewScore>
-                  <TextScore>Pontuação</TextScore>
-                  <TextScore>{item.score > 0 ? item.score : 0}</TextScore>
-                </ViewScore>
-                <ButtonDetails
-                  onPress={() => props.navigation.navigate('Result', { quizId: item.quizId, disputeId: item.id })
-                  }
-                >
-                  <Icon name="chevron-right" color="#000" size={30} />
-                </ButtonDetails>
-              </ViewRight>
-            </ItemList>
-          )}
-        />
+        {disputes.length > 0 ? (
+          <FlatList
+            data={disputes}
+            keyExtractor={item => `${item.id}`}
+            onRefresh={fetchData}
+            refreshing={loading}
+            renderItem={({item, index}) => (
+              <ItemList>
+                <ViewLeft>
+                  <TitleItem>{item.Quiz.name}</TitleItem>
+                  <DateDispute>
+                    {formatToTimeZone(item.createdAt, 'DD/MM/YY | HH:mm', {
+                      timeZone: 'America/Sao_Paulo',
+                    })}
+                  </DateDispute>
+                </ViewLeft>
+                <ViewRight>
+                  <ViewScore>
+                    <TextScore>Pontuação</TextScore>
+                    <TextScore>{item.score > 0 ? item.score : 0}</TextScore>
+                  </ViewScore>
+                  <ButtonDetails
+                    onPress={() =>
+                      props.navigation.navigate('Result', {
+                        quizId: item.quizId,
+                        disputeId: item.id,
+                      })
+                    }>
+                    <Icon name="chevron-right" color="#000" size={30} />
+                  </ButtonDetails>
+                </ViewRight>
+              </ItemList>
+            )}
+          />
+        ) : (
+          <ViewNotQuiz>
+            <TextNotQuiz>Histórico não disponível</TextNotQuiz>
+            <ButtonUpdate onPress={fetchData}>
+              <TextUpdate>Atualizar</TextUpdate>
+            </ButtonUpdate>
+          </ViewNotQuiz>
+        )}
       </ViewList>
     </Container>
   );
